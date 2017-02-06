cd83a90e7d958782b030e61ab5ca8a8ad37a3003
Monica Powell <mmp2181@columbia.edu>
2017-02-05 19:57:55 -0500

    added comments and README.md for additional context

---
+++ README.md
@@ -0,0 +1,3 @@
+# flask_weather
+
+Flask app to auto-detect local weather (temperature and chance of rain) based off of user's IP address.
--- detect_location_weather.py
+++ detect_location_weather.py
@@ -13,14 +13,19 @@ app = Flask(__name__)
 
 def location():
 
+    # get the user's IP address
     user_ip = ipgetter.myip()
     print(user_ip)
+
+    # get location information based off of IP address
     url = 'http://freegeoip.net/json/'+user_ip
     r = requests.get(url)
     js = r.json()
     city = js['city']
     state = js['region_name']
     ip_coordinates = str(js['latitude']) + "," + str(js['longitude'])
+
+    #pass the coordinates and city name to the route that gets weather info
     return redirect(url_for('weather', ip_coordinates=ip_coordinates, city=city, state=state))
     # return "hah"
 @app.route('/weather/<ip_coordinates>/<city>/<state>')
@@ -28,11 +33,16 @@ def location():
 def weather(ip_coordinates, city, state):
     weather_key = config.weather_key
     degree_sign= u'\N{DEGREE SIGN}'
+
+    # request weather info from the weather API
     # format for weather api request = https://api.darksky.net/forecast/[key]/[latitude],[longitude]
     response = requests.get('https://api.forecast.io/forecast/' + weather_key + '/' + ip_coordinates)
     data = response.json()
+
     temperature = str(int(data['currently']['temperature']))
     RAIN_WARNING = str(data['daily']['data'][0]['precipProbability']) + "% chance of rain."
+
+    #print out a statement with the current weather info + location that was used/detected
     return("Right now in "+ city + ", " + state + " it is " + temperature +  degree_sign + " and there  is a " + RAIN_WARNING)
 if __name__ == '__main__':
     app.run()
