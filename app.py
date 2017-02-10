b37a0105ccad768a416b3ea23e76b532fd3d5628
Monica Powell <mmp2181@columbia.edu>
2017-02-10 00:16:00 -0500

    removed request and replaced with urlopen

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, from urllib2 import urlopen, redirect, url_for
 from geoip import geolite2
 from geoip import open_database
 import urllib.request as ur
