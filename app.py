6a2a4605a61526954eb0e99e69dc1bef12dfd200
Helmut Irle <helmut@devigninnovation.com>
2018-10-13 10:45:14 +0200

    Further pep8 things.

--- app.py
+++ app.py
@@ -11,12 +11,16 @@ app.secret_key = config.secret_key
 def location():
     if (session and session['ip_info']):
         data = session['ip_info']
-        return redirect(url_for('weather', city=data['city'], state=data['state']))
+        return redirect(
+            url_for('weather', city=data['city'], state=data['state'])
+        )
     else:
         data = get_ip_info()
         if data['success']:
             session['ip_info'] = data
-            return redirect(url_for('weather', city=data['city'], state=data['state']))
+            return redirect(
+                url_for('weather', city=data['city'], state=data['state'])
+            )
         else:
             return redirect(url_for('error_page'))
 
@@ -32,16 +36,20 @@ def celsius():
 @app.route('/weather/<city>/<state>')
 def weather(city, state):
     weather_key = config.weather_key
-    degree_sign = u'\N{DEGREE SIGN}'
+    # degree_sign = u'\N{DEGREE SIGN}'
 
     data = session['ip_info']
     # request weather info from the weather API
-    # format for weather api request = https://api.darksky.net/forecast/[key]/[latitude],[longitude]
+    # format for weather api request =
+    # https://api.darksky.net/forecast/[key]/[latitude],[longitude]
     response = requests.get(
-        'https://api.forecast.io/forecast/%s/%s%s' % (weather_key, data['ip_coords'], celsius())
+        'https://api.forecast.io/forecast/%s/%s%s' % (
+            weather_key, data['ip_coords'], celsius()
+        )
     )
     data = response.json()
-    # data['hourly'] contains hourly data with the time formatted as Epoch Unix Time - should look into how to display hourly data in weather.html
+    # data['hourly'] contains hourly data with the time formatted as Epoch
+    # Unix Time - should look into how to display hourly data in weather.html
     # data['hourly']
     weather_icon = str(data['currently']['icon'])
     temperature = str(int(data['currently']['temperature']))
@@ -50,21 +58,29 @@ def weather(city, state):
     if RAIN_WARNING == 0:
         rain_commentary = "there is a no chance of rain! It's a sunny day"
     elif 0 < RAIN_WARNING <= .5:
-        rain_commentary = "there is a slight chance of rain. You might want to grab an umbrella ☔"
+        rain_commentary = "there is a slight chance of rain. " \
+                          "You might want to grab an umbrella ☔"
     elif .5 < RAIN_WARNING < .75:
-        rain_commentary = "there is a high chance of rain. Grab an umbrella on your way out! ☔"
+        rain_commentary = "there is a high chance of rain. " \
+                          "Grab an umbrella on your way out! ☔"
     elif RAIN_WARNING == 1:
         rain_commentary = "it is raining right now!"
     else:
-        rain_commentary = "it is definitely going to rain today! GRAB YOUR UMBRELLA. ☔"
+        rain_commentary = "it is definitely going to rain today! " \
+                          "GRAB YOUR UMBRELLA. ☔"
     # str(data['daily']['data'][0]['precipProbability']) + "% chance of rain."
 
-    # print out a statement with the current weather info + location that was used/detected
-    # return("Right now in "+ city + ", " + state + " it is " + temperature +  degree_sign + " and there  is a " + RAIN_WARNING)
+    # print out a statement with the current weather info + location that was
+    # used/detected
+    # return("Right now in "+ city + ", " + state + " it is " + temperature
+    # + degree_sign + " and there is a " + RAIN_WARNING)
     location = {'city': city, 'state': state}
     weather_info = {'temperature': temperature, 'rain': rain_commentary}
     return render_template(
-        'weather.html', location=location, weather_info=weather_info, weather_icon=weather_icon
+        'weather.html',
+        location=location,
+        weather_info=weather_info,
+        weather_icon=weather_icon
     )
 
 
