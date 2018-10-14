7d32d25d6ca34fd23757581b0798869d35294df4
Arn Borres <arnborres@outlook.com>
2018-10-13 22:59:26 -0700

    weather commentary added

--- app.py
+++ app.py
@@ -57,19 +57,28 @@ def weather(city, state):
     temperature = str(int(data['currently']['temperature']))
     RAIN_WARNING = data['daily']['data'][0]['precipProbability']
 
+    rain_level = {
+                    0:  "there is a slight chance of rain. " \
+                        "You might want to grab an umbrella ☔",
+                    1:  "there is a high chance of rain. " \
+                        "Grab an umbrella on your way out! ☔",
+                    2:  "it is raining right now!",
+                    3:  "it is definitely going to rain today! " \
+                        "GRAB YOUR UMBRELLA. ☔"
+                }
+
     if RAIN_WARNING == 0:
-        rain_commentary = "there is a no chance of rain! It's a sunny day"
+        rain_commentary = weather_commentary(temperature)
     elif 0 < RAIN_WARNING <= .5:
-        rain_commentary = "there is a slight chance of rain. " \
-                          "You might want to grab an umbrella ☔"
+        rain_commentary = rain_level[0]
     elif .5 < RAIN_WARNING < .75:
-        rain_commentary = "there is a high chance of rain. " \
-                          "Grab an umbrella on your way out! ☔"
+        rain_commentary = rain_level[1]
     elif RAIN_WARNING == 1:
-        rain_commentary = "it is raining right now!"
+        rain_commentary = rain_level[2]
     else:
-        rain_commentary = "it is definitely going to rain today! " \
-                          "GRAB YOUR UMBRELLA. ☔"
+        rain_commentary = rain_level[3]
+    
+
     # str(data['daily']['data'][0]['precipProbability']) + "% chance of rain."
 
     # print out a statement with the current weather info + location that was
@@ -104,6 +113,32 @@ def get_ip_info():
         'ip_coords': str(js['lat']) + ", " + str(js['lon'])
     }
 
+def weather_commentary(temperature):
+    temperature = int(temperature)
+    temperature_level = {
+                0:  "it's scorching hot. Stay cool!",
+                1:  "it's hot and sunny. Don't forget that sunscreen!",
+                2:  "it's nice and warm today. Time to flex those flip-flops",
+                3:  "a cup of hot cappucino would be nice on this cool weather",
+                4:  "it's gonna be cold today. Make sure you keep yourself warm!",
+                5:  "winter is here! Brrrrrrr",
+                6:  "it's Freezing Cold."
+    }
+
+    if temperature >= 95:
+        return temperature_level[0]
+    elif 80 <= temperature <= 94:
+        return temperature_level[1]
+    elif 69 <= temperature <= 79:
+        return temperature_level[2]
+    elif 59 <= temperature <= 68:
+        return temperature_level[3]
+    elif 40 <= temperature <= 57:
+        return temperature_level[4]
+    elif 25 <= temperature <= 39:
+        return temperature_level[5]
+    elif temperature <= 24:
+        return temperature_level[6]
 
 if __name__ == '__main__':
     app.run()
