556be68cc9b955b5b5f3ee4cb954582891ca342e
Monica Powell <monica@aboutmonica.com>
2018-10-23 07:22:24 -0400

    Revert "Merge branch 'master' into Issue28_Unit_Test_Weather_Information"
    This reverts commit e58d580238f19fd11c2dcc2cfaa79247375876e9, reversing
    changes made to e38d2c873972bed76af3ee8bb576480aa53878c7.

--- app.py
+++ app.py
@@ -36,60 +36,21 @@ def celsius():
 
 @app.route('/weather/<city>/<state>')
 def weather(city, state):
-
     weather_icon, temperature, RAIN_WARNING = get_weather_data(city, state)
-    weather_key = os.environ['weather_key']
-
-    if 'ip_info' in session:
-        data = session['ip_info']
-        geo_info = data['ip_coords']
-    else:
-        geo_info = get_geo_info(city, state)
-
-    if geo_info is None:
-        return error_page('GEO info Missing!')
-    
-    # request weather info from the weather API
-    # format for weather api request =
-    # https://api.darksky.net/forecast/[key]/[latitude],[longitude]
-    response = requests.get(
-        'https://api.forecast.io/forecast/%s/%s%s' % (
-            weather_key, geo_info, celsius()
-        )
-    )
-    data = response.json()
-
-    # data['hourly'] contains hourly data with the time formatted as Epoch
-    # Unix Time - should look into how to display hourly data in weather.html
-    # data['hourly']
-
-   # weather_icon = str(data['currently']['icon'])
-   # temperature = str(int(data['currently']['temperature']))
-   # RAIN_WARNING = data['daily']['data'][0]['precipProbability']
-
-
-    rain_level = {
-                    0:  "there is a slight chance of rain. " \
-                        "You might want to grab an umbrella ☔",
-                    1:  "there is a high chance of rain. " \
-                        "Grab an umbrella on your way out! ☔",
-                    2:  "it is raining right now!",
-                    3:  "it is definitely going to rain today! " \
-                        "GRAB YOUR UMBRELLA. ☔"
-                }
 
     if RAIN_WARNING == 0:
-        rain_commentary = weather_commentary(temperature)
+        rain_commentary = "there is a no chance of rain! It's a sunny day"
     elif 0 < RAIN_WARNING <= .5:
-        rain_commentary = rain_level[0]
+        rain_commentary = "there is a slight chance of rain. " \
+                          "You might want to grab an umbrella ☔"
     elif .5 < RAIN_WARNING < .75:
-        rain_commentary = rain_level[1]
+        rain_commentary = "there is a high chance of rain. " \
+                          "Grab an umbrella on your way out! ☔"
     elif RAIN_WARNING == 1:
-        rain_commentary = rain_level[2]
+        rain_commentary = "it is raining right now!"
     else:
-        rain_commentary = rain_level[3]
-    
-
+        rain_commentary = "it is definitely going to rain today! " \
+                          "GRAB YOUR UMBRELLA. ☔"
     # str(data['daily']['data'][0]['precipProbability']) + "% chance of rain."
 
     # print out a statement with the current weather info + location that was
@@ -134,38 +95,8 @@ def get_ip_info():
         'success': js['status'] == 'success',
         'city': js['city'],
         'state': js['regionName'],
-        'ip_coords': get_geo_str(js['lat'], js['lon'])
+        'ip_coords': str(js['lat']) + ", " + str(js['lon'])
     }
-
-
-def get_geo_info(city, state):
-    # get location information based off of city/state
-    url = 'http://www.datasciencetoolkit.org/maps/api/geocode/json?sensor=false&address=%s,%s' % (city, state)
-    response = requests.get(url)
-    js = response.json()
-
-    if js['status'] == 'OK':
-        lat = js['results'][0]['geometry']['location']['lat']
-        lon = js['results'][0]['geometry']['location']['lng']
-        return get_geo_str(lat, lon)
-    else:
-        return None;
-
-def get_geo_str(lat, lon):
-    return str(lat) + "," + str(lon)
-
-def weather_commentary(temperature):
-    temperature = int(temperature)
-    temperature_level = {
-                0:  "it's scorching hot. Stay cool!",
-                1:  "it's hot and sunny. Don't forget that sunscreen!",
-                2:  "it's nice and warm today. Time to flex those flip-flops",
-                3:  "a cup of hot cappucino would be nice on this cool weather",
-                4:  "it's gonna be cold today. Make sure you keep yourself warm!",
-                5:  "winter is here! Brrrrrrr",
-                6:  "it's Freezing Cold."
-    }
-
 
 def get_weather_data(city, state):
     weather_key = os.environ['weather_key']
@@ -191,22 +122,6 @@ def get_weather_data(city, state):
     RAIN_WARNING = data['daily']['data'][0]['precipProbability']
 
     return weather_icon, temperature, RAIN_WARNING
-
-    if temperature >= 95:
-        return temperature_level[0]
-    elif 80 <= temperature <= 94:
-        return temperature_level[1]
-    elif 69 <= temperature <= 79:
-        return temperature_level[2]
-    elif 59 <= temperature <= 68:
-        return temperature_level[3]
-    elif 40 <= temperature <= 57:
-        return temperature_level[4]
-    elif 25 <= temperature <= 39:
-        return temperature_level[5]
-    elif temperature <= 24:
-        return temperature_level[6]
-
 
 if __name__ == '__main__':
     app.run()
